2802254c6a43ebdb6ef4723494e1bca6af0673b2
kenenisa1 <kenenisamiesob@gmail.com>
2026-04-18 01:21:21 +0300

    fix tailwindcss config issue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Check if your files are .jsx or .js
-  ],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        'smuc-emerald': '#064e3b',
-        'smuc-gold': '#d4af37',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
