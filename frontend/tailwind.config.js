e4a7c2bdc6c6db5ff4975a643b2f24ed1e95c97d
kenenisa1 <kenenisamiesob@gmail.com>
2026-04-18 00:11:03 +0300

    Update Tailwindcss setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Check if your files are .jsx or .js
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'smuc-emerald': '#064e3b',
+        'smuc-gold': '#d4af37',
+      },
+    },
   },
   plugins: [],
 }
-
