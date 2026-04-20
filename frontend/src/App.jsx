919af2f2660f24259d922a2e28704dffb8ec2111
Gurmesa <gurmesakedir96@gmail.com>
2026-04-20 12:09:05 +0300

    This Footer Part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import Register from "./pages/Register";
 import HelpPage from "./pages/HelpPage";
+import Navbar from "./components/headerandfooter/Navbar";
+import Footer from "./components/headerandfooter/Footer";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
       <div>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Register />} />
           <Route path="/help" element={<HelpPage />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   )
